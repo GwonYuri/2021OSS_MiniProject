c9f10a113e309e6581a21e7f0785cbc282fedf7a
GwonYuri <21900048@gmail.com>
2021-04-15 14:18:19 +0900

    C4

--- manager.h
+++ manager.h
@@ -6,5 +6,8 @@ int selectMenu();
 int selectDataNo(Product p[], int product_count);
 void saveData(Product *p, int product_count);
 int loadData(Product *p);
+void searchName(Product *p, int product_count);
+void searchPrice(Product *p,int product_count);
+void searchGrade(Product *p,int product_count);
 
 #endif
