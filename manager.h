53e49aadd963498ad99e2b83e6f355da85d959c5
GwonYuri <21900048@gmail.com>
2021-04-15 14:13:06 +0900

    C2

--- manager.h
+++ manager.h
@@ -2,7 +2,9 @@
 
 #define _MANAGER_H
 #include "product.h" 
-int selectMenu(); // 사용자가 원하는 관리 기능을 선택하는 함수
-int selectDataNo(Product p[], int product_count); // 사용자가 원하는 상품 번호를 반환해주는 함수
+int selectMenu(); 
+int selectDataNo(Product p[], int product_count);
+void saveData(Product *p, int product_count);
+int loadData(Product *p);
 
 #endif
